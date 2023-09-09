34cfc8991d3d06316ed47fa2360fd9f62891a60e
jeeban <jeebanjyoti193@gmail.com>
2023-09-09 11:45:39 +0530

    first commit

---
+++ Phase3Section2.36/Phase3Section2.36/Views/Home/CustomHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Phase3Section3._36.Views.Home
+{
+    public class CustomHelper
+    {
+        public static IHtmlContent File(string id)
+        {
+            TagBuilder tb = new TagBuilder("input");
+            tb.Attributes.Add("type", "file");
+            tb.Attributes.Add("id", id);
+            return new HtmlString(tb.ToString());
+        }
+    }
+}
